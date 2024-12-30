f8d3f680d5f08bec0ff77f8067c575b985b13bd9
Juba <juba98@gmail.com>
2024-12-30 15:22:06 +0100

    shiny v1.2

--- shiny_scRNAseq.R
+++ shiny_scRNAseq.R
@@ -236,7 +236,7 @@ options(shiny.maxRequestSize = 900*1024^2)
 ui <- fluidPage(
   # theme = bs_theme(bootswatch = "flatly"),
   theme = shinytheme("flatly"),
-  h2("I- Importing the data",
+  h1("I- Importing the data",
      style = "color:gold ; font-weight:700 ; background-color:black ; margin-top:10px ; margin-bottom:20px"),
   
   ## I- Importing data   --------------------------------------------------------
@@ -252,8 +252,8 @@ ui <- fluidPage(
   ),
   
   ## II- Metadata selection   ----------------------------------------------------
-  h2("II- Metadata selection",
-     style = "color:gold ; font-weight:700 ; background-color:black ; margin-top:150px ; margin-bottom:20px"),
+  h1("II- Metadata selection",
+     style = "color:gold ; font-weight:700 ; background-color:black ; margin-top:200px ; margin-bottom:20px"),
   fluidRow(
     column(width = 7,
            selectInput(inputId = "Sel.metadata",
@@ -265,18 +265,19 @@ ui <- fluidPage(
            plotOutput("plt1", height = "500px")),
     column(width = 3,
            offset = 1,
-           actionButton("act1", "View the summary", class = "btn-lg btn-primary", width = "100%"),
+           actionButton("act1", "View the summary", class = "btn-lg btn-primary", 
+                        width = "100%", style = "font-size:28px ; color:gold ; font-weight:600 ; border-radius:15px"),
            p("", style = "margin-top:30px"),
            tableOutput("tab1"))
   ),
   
   ## III- DEA   ------------------------------------------------------------------
-  h2("III- Differential expression analysis",
-     style = "color:gold ; font-weight:700 ; background-color:black ; margin-top:150px ; margin-bottom:20px"),
+  h1("III- Differential expression analysis",
+     style = "color:gold ; font-weight:700 ; background-color:black ; margin-top:200px ; margin-bottom:20px"),
   
   ### 1- Making the contrasts   --------------------------------------------------
   p("1- Select the contrasts",
-    style = "color:darkred ; font-weight:600 ; font-size:140% ; background-color:gold ; margin-bottom:20px"),
+    style = "color:darkred ; font-weight:600 ; font-size:160% ; background-color:gold ; margin-bottom:20px"),
   fluidRow(
     column(width = 4,
            p("From the selected metadata above, choose the cluster you want to test and against which cluster ! 
@@ -290,10 +291,10 @@ ui <- fluidPage(
   ),
   
   ### 2- Setting the parameters   ------------------------------------------------
-  p("2- Setting the parameters (prior DEA)",
-    style = "color:darkred ; font-weight:600 ; font-size:140% ; background-color:gold ; margin-top: 20px ; margin-bottom:20px"),
+  p("2- Setting the parameters & Run the DEA",
+    style = "color:darkred ; font-weight:600 ; font-size:160% ; background-color:gold ; margin-top: 50px ; margin-bottom:20px"),
   fluidRow(
-    column(width = 2.5,
+    column(width = 3,
            p("Initial parameters", style = "color:darkgreen ; font-weight:600 ; font-size:120% ; margin-bottom:30px"),
            div(
              tags$span(
@@ -301,36 +302,84 @@ ui <- fluidPage(
              "Log2FC min.threshold ",
              icon("info-circle", id= "info-numeric", style = "color:midnightblue ; cursor:pointer")
              ),
-             numericInput("log2fc",NULL,value = 0.5, min = 0, max = 3, step = 0.1, width = "200px")
+             numericInput("log2fc",NULL,value = 0.5, min = 0, max = 3, step = 0.1, width = "100px")
            ),
            bsTooltip(id = "info-numeric",
                      title = "Define the minimum Log2FC threshold to be applied before running the DEA (in absolute value).",
                      placement = "top", trigger = "hover", options = list(container = "body")),
-           
            div(
              tags$span(
                style = "font-weight:600 ; font-size:110%",
                "Min.pct threshold ",
                icon("info-circle", id= "info-numeric2", style = "color:midnightblue ; cursor:pointer")
              ),
-             numericInput("min.pct",NULL,value = 0.1, min = 0, max = 1, step = 0.05, width = "200px")
+             numericInput("min.pct",NULL,value = 0.1, min = 0, max = 1, step = 0.05, width = "100px")
            ),
            bsTooltip(id = "info-numeric2",
                      title = "Define the minimum percentage of cells within any cluster that have to express the gene",
+                     placement = "top", trigger = "hover", options = list(container = "body")),
+           div(
+             tags$span(
+               style = "font-weight:600 ; font-size:110%",
+               "Min.diff.pct",
+               icon("info-circle", id= "info-numeric3", style = "color:midnightblue ; cursor:pointer")
+             ),
+             numericInput("min.diff.pct",NULL,value = 0.1, min = 0, max = 1, step = 0.025, width = "100px")
+           ),
+           bsTooltip(id = "info-numeric3",
+                     title = "Define the minimum difference in the fraction of detection between the 2 groups for each gene",
                      placement = "top", trigger = "hover", options = list(container = "body")),
            
            
            p("Additional filtering", 
              style = "color:darkgreen ; font-weight:600 ; font-size:120% ; margin-bottom:30px ; margin-top:20px"),
+           div(
+             tags$span(
+               style = "font-weight:600 ; font-size:110%",
+               "Filter Up log2FC ",
+               icon("info-circle", id= "info-slider1", style = "color:midnightblue ; cursor:pointer")
+             ),
+             sliderInput("log2fc_up",NULL,value = c(0.5,10), min = 0, max = 10, step = 0.25, width = "250px")
+           ),
+           bsTooltip(id = "info-slider1",
+                     title = "Filter the up regulated genes by the selected range of log2FC",
+                     placement = "top", trigger = "hover", options = list(container = "body")),
+           div(
+             tags$span(
+               style = "font-weight:600 ; font-size:110%",
+               "Filter Down log2FC ",
+               icon("info-circle", id= "info-slider2", style = "color:midnightblue ; cursor:pointer")
+             ),
+             sliderInput("log2fc_down",NULL,value = c(-10,-0.5), min = -10, max = 0, step = 0.25, width = "250px")
+           ),
+           bsTooltip(id = "info-slider2",
+                     title = "Filter the down regulated genes by the selected range of log2FC",
+                     placement = "top", trigger = "hover", options = list(container = "body")),
            
-           
-           sliderInput("min.pct", "Min.pct threshold", min = 0, max = 1, step = 0.05, value = 0.1, width = "200px"),
-           p("Define the minimum percentage of cells within any cluster that have to express the gene",
-             style = "color:darkgray ; font-style:italic ; font-size:90%")),
-    column(width = 2.5,
-           sliderInput("logfc_filt_up", "Filter Up log2FC", min = 0, max = 12, value = c(0,12), step = 0.1, width = "250px"),
-           sliderInput("logfc_filt_down", "Filter down log2FC", min = -12, max = 0, value = c(-12,0), step = 0.1, width = "250px"),
-           sliderInput("pct.1_filt", "Filter pct.1", min = 0, max = 1, value = c(0,1), width = "250px"))
+           div(
+             style = "margin-top:20px ; margin-left:20px ; margin-bottom:20px",
+             actionButton("act2","Run DEA", class= "btn-primary", width = "70%", 
+                          style = "font-size:26px ; font-weight:600 ; border-radius:10px")
+           )),
+    
+    ### 3- Results   ------------------------------------------------
+    column(width = 9,
+           navbarPage(
+             "DEA Results",
+             
+             #### p1: Datatable     ----------------------------------------------
+             tabPanel(
+               "Table",
+               fluidRow(
+                 dataTableOutput("tab2"),
+                 div(
+                   style = "margin-top:30px ; margin-bottom:30px",
+                   downloadButton("download.tab","Download as excel", class = "btn-sm",
+                                  style = "font-size:20px ; background-color:darkgreen ; padding:5px 150px ; border-radius:10px")
+                 )
+               )
+             )
+           ))
   )
 )
 
@@ -425,6 +474,89 @@ server <- function(input, output, session){
       my_umap_theme()+
       guides(colour = guide_legend(override.aes = list(size = 3.5)))
   }, height = 500)
+  
+  
+  ## 2- Setting the parameters      ------------------------------------------------
+  observeEvent(input$act2, {
+    showNotification("DEA is running >>> ", duration = 10, type = "message")
+    message("==============================================")
+    message("DEA is running with the following parameters: ")
+    message("==============================================")
+    message(sprintf("%-25s: %s", "Selected metadata", input$Sel.metadata))
+    message(sprintf("%-25s: %s", "Ident.1", input$ident.1))
+    message(sprintf("%-25s: %s", "Ident.2", input$ident.2))
+    message(sprintf("%-25s: %s", "Min.log2FC.threshold", input$log2fc))
+    message(sprintf("%-25s: %s", "Min.pct.threshold", input$min.pct))
+    message(sprintf("%-25s: %s", "Min.diff.pct", input$min.diff.pct))
+    message("==============================================")
+  })
+  
+  observeEvent(input$log2fc, {
+    updateSliderInput(session, "log2fc_up", value = c(input$log2fc,10))
+    updateSliderInput(session, "log2fc_down", value = c(-10,-(input$log2fc)))
+  })
+  
+  
+  ## 3- Running DEA     ----------------------------------------------------------
+  DEA_results <- eventReactive(input$act2, {
+    req(seurat())
+    seurat <- seurat()
+    Idents(seurat) <- as.factor(seurat@meta.data[[input$Sel.metadata]])
+    seurat %>% 
+      FindMarkers(ident.1 = input$ident.1,
+                  ident.2 = input$ident.2,
+                  only.pos = FALSE,
+                  min.pct = input$min.pct,
+                  min.diff.pct = input$min.diff.pct,
+                  logfc.threshold = input$log2fc,
+                  verbose = TRUE) %>% 
+      dplyr::mutate(Diff_pct = round((pct.1 - pct.2), 3),
+                    avg_log2FC = round(avg_log2FC, 3)) %>% 
+      dplyr::filter(p_val_adj < 0.05,
+                    avg_log2FC >= input$log2fc_up[1] & avg_log2FC <= input$log2fc_up[2] |
+                      avg_log2FC >= input$log2fc_down[1] & avg_log2FC <= input$log2fc_down[2]) %>% 
+      dplyr::arrange(desc(avg_log2FC)) %>%
+      dplyr::select(avg_log2FC, pct.1, pct.2, Diff_pct, p_val_adj) %>%
+      rownames_to_column(var = "Genes") 
+  })
+  
+  observeEvent(DEA_results(), {
+    res <- DEA_results()
+    max_fc <- max(res[["avg_log2FC"]])
+    min_fc <- min(res[["avg_log2FC"]])
+    message("______________________________________________________")
+    message("Analysis completed successfully !!!")
+    message("==============================================")
+    message(sprintf("%-25s: %s", "Total Nb of DEGs", nrow(res)))
+    message(sprintf("%-25s: %s", "Nb of Up genes", nrow(res[which(res$avg_log2FC > 0),])))
+    message(sprintf("%-25s: %s", "Nb of Down genes", nrow(res[which(res$avg_log2FC < 0),])))
+    updateSliderInput(session, "log2fc_up", value = c(input$log2fc, max_fc))
+    updateSliderInput(session, "log2fc_down", value = c(min_fc, -(input$log2fc)))
+  })
+  
+  ## 4- Displaying results      ---------------------------------------------------
+  ### p1: datatable     -----------------------------------------------------------
+  output$tab2 <- renderDataTable({
+    req(DEA_results())
+    DEA_results()
+  }, options = list(pageLength = 5, scrollX = T))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # X- Downloading      -----------------------------------------------------------
+  output$download.tab <- downloadHandler(
+    filename = function(){paste0("DEA_res_", input$ident.1,"_vs_",input$ident.2,".xlsx")},
+    content = function(file){write_xlsx(DEA_results(), path = file)}
+  )
+  
+  
   
 }
 
